88b49a15503fd1a0f43a27d066ccd8f8fff02406
tfavazza <tfavazza@users.noreply.github.com>
2019-03-07 21:18:32 -0500

    Updates

--- precache-manifest.84edaadbfe08283edae14fbd21870823.js
+++ precache-manifest.84edaadbfe08283edae14fbd21870823.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "b48765f96e4371f9e78e6330202a951f",
-    "url": "/scl-season-4/static/media/balcony-blurred.b48765f9.png"
+    "revision": "77c8e6fa9d8d72295786b1dba966b60d",
+    "url": "/scl-season-4/static/media/moderne-blurred.77c8e6fa.png"
   },
   {
-    "revision": "df249a4136d348829eb1",
+    "revision": "ddf03da0bbce084f79de",
     "url": "/scl-season-4/static/css/main.ac3a3c0b.chunk.css"
   },
   {
@@ -64,48 +64,48 @@ self.__precacheManifest = [
     "url": "/scl-season-4/static/media/scl_badge_challenger.0dab3afc.svg"
   },
   {
-    "revision": "a4b56e119a5a50f45b4b75b430094205",
-    "url": "/scl-season-4/static/media/courtyard-blurred.a4b56e11.png"
-  },
-  {
     "revision": "be2214c3b2c065a094500829abea74e3",
     "url": "/scl-season-4/static/media/gallery-blurred.be2214c3.png"
+  },
+  {
+    "revision": "b48765f96e4371f9e78e6330202a951f",
+    "url": "/scl-season-4/static/media/balcony-blurred.b48765f9.png"
   },
   {
     "revision": "f78769242fb5c0ec2f775f959c8ee8cd",
     "url": "/scl-season-4/static/media/ballroom-blurred.f7876924.png"
   },
   {
-    "revision": "307ef4f6e791b6c5e9b9b6ecfcb4daf2",
-    "url": "/scl-season-4/static/media/high-rise-blurred.307ef4f6.png"
-  },
-  {
-    "revision": "df249a4136d348829eb1",
-    "url": "/scl-season-4/static/js/main.df249a41.chunk.js"
-  },
-  {
     "revision": "9361235ed229ef69084a9930dec18552",
     "url": "/scl-season-4/static/media/library-blurred.9361235e.png"
   },
   {
-    "revision": "77c8e6fa9d8d72295786b1dba966b60d",
-    "url": "/scl-season-4/static/media/moderne-blurred.77c8e6fa.png"
+    "revision": "ddf03da0bbce084f79de",
+    "url": "/scl-season-4/static/js/main.ddf03da0.chunk.js"
+  },
+  {
+    "revision": "307ef4f6e791b6c5e9b9b6ecfcb4daf2",
+    "url": "/scl-season-4/static/media/high-rise-blurred.307ef4f6.png"
   },
   {
     "revision": "29cb205402fe1e2604c0ff87b20c03a3",
     "url": "/scl-season-4/static/media/pub-blurred.29cb2054.png"
   },
   {
-    "revision": "38f1c428410a32c5e033dcfcb8d36738",
-    "url": "/scl-season-4/static/media/veranda-blurred.38f1c428.png"
-  },
-  {
     "revision": "f77412d3c43efbe1acd9dd41c63c1c41",
     "url": "/scl-season-4/static/media/terrace-blurred.f77412d3.png"
   },
   {
+    "revision": "a4b56e119a5a50f45b4b75b430094205",
+    "url": "/scl-season-4/static/media/courtyard-blurred.a4b56e11.png"
+  },
+  {
     "revision": "10e11d88cee0e11dadc6edaa46b7a182",
     "url": "/scl-season-4/static/media/teien-blurred.10e11d88.png"
+  },
+  {
+    "revision": "38f1c428410a32c5e033dcfcb8d36738",
+    "url": "/scl-season-4/static/media/veranda-blurred.38f1c428.png"
   },
   {
     "revision": "09f8612f418bb223aab499ee4d5e3499",
@@ -156,7 +156,7 @@ self.__precacheManifest = [
     "url": "/scl-season-4/static/css/2.2a09930e.chunk.css"
   },
   {
-    "revision": "cf27267eb97d0dcd5477e98a8199534f",
+    "revision": "b0a06f6bad19b8ca255f1cefb4821302",
     "url": "/scl-season-4/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/scl-season-4/precache-manifest.941a33bc7b927bc6602e8425fe27ef4b.js"
+  "/scl-season-4/precache-manifest.84edaadbfe08283edae14fbd21870823.js"
 );
 
 workbox.clientsClaim();
